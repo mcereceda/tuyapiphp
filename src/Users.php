780104bf6e4778bbf0b4e952e304963968606fe3
Marcelo Cereceda <mcereceda@naturalphone.cl>
2021-11-05 15:14:46 -0300

    05-11

---
+++ src/Users.php
@@ -0,0 +1,25 @@
+<?php
+
+	namespace tuyapiphp;
+
+	Class Users
+	{
+		public function __construct( $config, $token )
+		{
+			$this->_config = $config;
+			$this->_token = $token;
+		}
+		
+		public function __call( $name , $args = [ ] )
+		{
+			$request = new Caller( $this->_config , $this->_endpoints , $this->_token );
+			return $request->send( $name , $args );
+		}
+		
+		protected $_token = '';
+
+		protected $_endpoints = 
+		[
+			'get_users'		=> '/v1.0/iot-02/users'
+		];
+	}
